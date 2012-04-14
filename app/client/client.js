de8346d7a23eb5365c36904571f262ae6fb45434
zachleat <zachleatherman@gmail.com>
2012-04-14 17:34:21 -0500

    Adding jQuery Mobile and splash page template.

---
+++ app/client/client.js
@@ -0,0 +1,43 @@
+// Template.splash.greeting = function () {
+//   return "Welcome to obh.";
+// };
+
+var OBH = {
+  templates: {
+    bounty: function() {
+      return Template.bounty;
+    },
+    splash: function() {
+      Template.splash.events = {
+        'click .single': function() {
+          OBH.addPage( 'bounty' );
+          OBH.changePage( 'bounty' );
+        }
+      };
+
+      return Template.splash;
+    }
+  },
+  changePage: function( name )
+  {
+    $.mobile.changePage( $( '#' + name ) );
+  },
+  addPage: function( name, vars ) {
+    var template = OBH.templates[ name ];
+
+    if ( template ) {
+      $( document.body ).append( Meteor.ui.render( template( vars ) ) );
+    }
+  },
+  addPages: function( names ) {
+    
+    OBH.addPage()
+  }
+};
+
+Meteor.startup(function () {
+  //$( '<link>' ).attr( 'href', 'jquery.mobile-1.1.0.css' ).appendTo( 'head' );
+  $.getScript( 'jquery.mobile-1.1.0.js' );
+
+  OBH.addPage(s [ 'splash', 'bounty' ] );
+});
